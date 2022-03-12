08e096dc780ae59a907d5b52a16957dec682bc07
Ian Burzynski <23251244+iburzynski@users.noreply.github.com>
2022-03-11 21:39:20 -0500

    Add NixOS config

---
+++ .nixos/configuration.nix
@@ -0,0 +1,221 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec -a "$0" "$@"
+  '';
+in {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    kernelParams =
+      [ "acpi_rev_override" "mem_sleep_default=deep" "intel_iommu=igfx_off" "nvidia-drm.modeset=1" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+    extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp36s0.useDHCP = true;
+  networking.interfaces.wlp0s20f3.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    config = ''
+      Section "Device"
+        Identifier "Intel Graphics"
+        Driver     "intel"
+        Option     "TearFree" "true"
+        Option     "SwapbuffersWait" "true"
+        BusID      "PCI:0:2:0"
+      EndSection
+
+      Section "Device"
+        Identifier "nvidia"
+        Driver "nvidia"
+        BusID "PCI:1:0:0"
+        Option "AllowEmptyInitialConfiguration"
+      EndSection
+    '';
+    enable = true;
+    screenSection = ''
+      Option "metamodes" "nvidia-auto-select +0+0 { ForceFullCompositionPipeline=On }"
+      Option "AllowIndirectGLXProtocol" "off"
+      Option "TripleBuffer" "on"
+    '';
+    videoDrivers = ["nvidia"];
+    # Enable xmonad window manager
+    windowManager = {
+      xmonad.enable = true;
+      xmonad.enableContribAndExtras = true;
+    };
+    displayManager.defaultSession = "none+xmonad";
+  };
+  # services.xserver.displayManager.startx.enable = true;
+  
+  hardware.nvidia.modesetting.enable = true;
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+  hardware.nvidia.powerManagement.enable = true;
+
+  services.picom = {
+    enable = true;
+    inactiveOpacity = 0.8;
+    settings = {
+      "unredir-if-possible" = true;
+    };
+  }; 
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # Enable brightness control
+  programs.light.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.ian = {
+    isNormalUser = true;
+    initialPassword = "changeme";
+    extraGroups = [ "wheel" "video" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.fish;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+
+    # Utils
+    dconf
+    wget
+    git
+    efibootmgr
+    fortune
+    gnome.seahorse
+    light
+    monitor
+    networkmanager
+    scrot
+    unzip
+
+    # WM
+    dmenu
+    feh
+    haskellPackages.xmobar
+    lxappearance
+    stalonetray
+    xfce.xfce4-power-manager
+    xscreensaver
+
+    # Editors
+    vim 
+    vscode
+
+    # Web
+    firefox
+    brave
+    
+    # Comm
+    tdesktop
+    zoom-us
+
+    # Haskell stuff
+    ghc
+    haskell-language-server
+    hlint
+    cabal2nix
+    niv
+
+  ];
+  
+  # Fonts
+  fonts.fonts = with pkgs; [
+    mononoki
+    nerdfonts
+  ];
+
+  # Binary Cache for Haskell.nix
+  nix.binaryCachePublicKeys = [
+    "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+  ];
+  nix.binaryCaches = [
+    "https://hydra.iohk.io"
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.fish.enable = true;
+  programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+
+  # List services that you want to enable:
+  services.gnome.gnome-keyring.enable = true;
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.11"; # Did you read the comment?
+
+}
